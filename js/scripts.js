12b30350ab682d5cf7b62f2a0dcb608a849a6d73
Bright <bright@RUTHs-MacBook-Air.local>
2020-12-25 23:13:25 +0300

    Add a typewriter effect for the intro

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var asteroid = $('.one');
 
 $(document).ready(function(){
+    typeWriter()
     $("button").click(function(){
         asteroid.animate({
             left: '250px'
@@ -8,3 +9,20 @@ $(document).ready(function(){
         console.log(asteroid.position());
     })
 })
+
+//type the intro
+var i = 0;
+var txt = 'Are you a designer and you would like your work to be seen?';
+var speed = 60;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("intro").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+//get the current year for copyright mark
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById('year').innerHTML=year;
